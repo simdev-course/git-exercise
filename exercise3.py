906c83ccd8fa47d0dd4e684725b41bda038904fa
Omkar Bahiwal <ombahiwal@gmail.com>
2024-10-23 18:18:49 +0200

    check if is in interval and then return if the point is wihihn the shape (#6)

--- exercise3.py
+++ exercise3.py
@@ -32,8 +32,12 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= 0 and ll_px <= self._dx \
-            and ll_py >= 0 and ll_py <= self._dy
+        return (self._is_within_interval(ll_px, 0, self._dx) and self._is_within_interval(ll_py, 0, self._dy))
+    
+    # Task A solution
+    def _is_within_interval(self, value: float, low: float, high: float) -> bool:
+        return low <= value <= high
+
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
